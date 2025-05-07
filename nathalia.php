b62f97155a7f82801ce6f84956516175c9bac731
AKgalv1x <akgalvis@gmail.com>
2025-05-07 08:09:36 -0500

    pila_estructura

---
+++ nathalia.php
@@ -0,0 +1,19 @@
+<?php
+  $postres = array();
+
+  array_push($postres,"flan de chocolate");
+  array_push($postres,"tres leches");
+  array_push($postres,"postre cafe");
+  array_push($postres,"redvelbet");
+  array_push($postres,"tiramisu");
+  array_push($postres,"postre de limón");
+  array_push($postres,"postre de zanahoria");
+  array_push($postres,"pastel de frutas");
+  array_push($postres,"pastel de queso");
+
+  $tres_leches = array_pop($postres); //para sacar todos los elementos//
+
+  echo "el ultimo postre ingresado es " .$tres_leches;
+
+
+  ?>
